c9b48896d16236c3c996a5271caef6611f63d4ea
Ricardo Alf <rear_recinos@hotmail.com>
2026-09-01 10:29:47 -0600

    Revert "Add typeorm and basic configuration"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { validate } from './config/env.validation.js';
-import { databaseConfig } from './config/database.config.js';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validate,
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: () => databaseConfig(),
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  ValidationPipe,
-  BadRequestException,
-  ValidationError,
-} from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module.js';
-
-function flattenValidationErrors(errors: ValidationError[]): string[] {
-  return errors.flatMap((error) => {
-    const messages = Object.values(error.constraints ?? {});
-    if (error.children?.length) {
-      return flattenValidationErrors(error.children);
-    }
-    return messages;
-  });
-}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? 'http://localhost:3000',
-    credentials: false,
-  });
-
-  app.use(cookieParser());
-
-  app.setGlobalPrefix('api');
-
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-      stopAtFirstError: true,
-      exceptionFactory: (errors: ValidationError[]) => {
-        const formattedErrors = flattenValidationErrors(errors);
-        return new BadRequestException(formattedErrors);
-      },
-    }),
-  );
-
-  if (process.env.NODE_ENV !== 'production') {
-    const config = new DocumentBuilder()
-      .setTitle('Odonto System API')
-      .setDescription('API para gestión de consultorio odontológico')
-      .setVersion('1.0')
-      .build();
-
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('docs', app, document);
-  }
-
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
